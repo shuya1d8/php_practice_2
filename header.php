891f74f06a39687e3f18f6662f673e6128cf3e40
Shu <77907940+shuya1d8@users.noreply.github.com>
2023-01-24 23:44:51 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title> | -？ろごろご？-</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php 
+// エラー出力レベル（一部）
+// 全てのエラーを表示しない
+error_reporting(0);
+// 重大な実行時エラー。スクリプトの実行を中断する
+// error_reporting(E_ERROR);
+// 実行時の警告。スクリプトの実行は中断されない
+// error_reporting(E_WARNING);
+// スクリプト解析時のエラー。文法上の誤りがあるときに発生する
+// error_reporting(E_PARSE);
+// 実行時の注意。スクリプトに誤りがある疑いがあると発生する
+// error_reporting(E_NOTICE);
+// 全てのエラーを表示する
+// error_reporting(E_ALL);
+// 組み合わせ
+// error_reporting(E_ERROR | E_WARNING | E_PARSE | E_NOTICE);
+?>
+<header>
+    <h1>-？ろごろご？-</h1>
+
+</header>
