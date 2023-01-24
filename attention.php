95479d5eafd846988089493f3fff0de6bf297f52
Shu <77907940+shuya1d8@users.noreply.github.com>
2023-01-24 23:36:10 +0900

    Create attention.php

---
+++ attention.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>注意事項 | -？ろごろご？-</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+    <h1>-？ろごろご？-</h1>
+
+</header>
+
+<div id="wrapper">
+    <div id="attention">
+        <h1>注意事項</h1>
+        <br>
+        <p>2022年07月23日現在、お使いの媒体によってはこちらのサイトが崩れて表示されてしまう可能性がございます。</p>
+        <p>ユーザーの皆様には大変ご不便をおかけいたしますがご了承くださいますようお願い申し上げます。</p>
+        <br>
+        <p>また、同一端末の複数タグでログイン、新規登録しますと</p>
+        <p><b>最後に</b>ログイン、新規登録したアカウントの情報に上書きされますのでご了承ください。</p>
+        <img src="img/figure_gotai_touchi.png" alt="許して">
+
+    </div>
+
+</div>
+
+<footer>
+    <small>
+        &copy;-？ろごろご？-制作会社
+        <!-- <p><a href="attention.php" target="_blank">注意事項</a></p> -->
+    </small>
+</footer>
+</body>
+</html>
