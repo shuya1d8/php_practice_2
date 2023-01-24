b50d9b1e790896b1fe1bfcd9062ab7cb3fca5ae3
Shu <77907940+shuya1d8@users.noreply.github.com>
2023-01-24 23:34:23 +0900

    Create home.php

---
+++ home.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>HOME | -？ろごろご？-</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<header>
+    <h1>-？ろごろご？-</h1>
+    <nav id="globalNavi">
+        <ul>
+            <li class="current"><a href="home.php">HOME</a></li>
+            <!-- <li><a href="note.php">なにか</a></li> -->
+            <!-- <li><a href="idk.php">どれか</a></li> -->
+            <li><a href="contact.php">お問い合わせ</a></li>
+        </ul>
+        <ul>
+            <li><a href="new_reg.php">新規登録</a></li>
+            <li><a href="account.php">アカウント</a></li>
+        </ul>
+    </nav>
+
+</header>
+
+<div id="wrapper">
+    <div id="home">
+        <h1>HOME</h1>
+        
+        <div id="homeMain">
+            <h2>メイン</h2>
+            <p>このサイトは登録、お問い合わせができるだけのサイトです。</p>
+            <p>随時機能の追加は行うと思います。きっと。それにともないサイトのレイアウトも変更していきます。きっと。</p>
+            <p>なにか欲しい機能があればお問い合わせください。</p>
+            <h3>サブ</h3>
+            <p>ほぼ完成のようなものです。</p>
+            <p>ほぼ完成のようなものです。</p>
+            <p>ほぼ完成のようなものです。</p>
+            
+        </div>
+
+        <aside id="sidebar">
+            <section id="news">
+                <h3>お知らせ</h3>
+                <small>
+                    <dl>
+                        <dt>2022/08/25</dt>
+                        <dd>機能の大幅アップデートを開始しました。</dd>
+                        <dt>20XX/XX/XX</dt>
+                        <dd>HOMEを更新しました。</dd>
+                        <dt>20XX/XX/XX</dt>
+                        <dd><strike>？ろごろご？</strike>を開設しました。</dd>
+                    </dl>
+                </small>
+            </section>
+            <section id="relation">
+                <h3>関連リンク</h3>
+                <ul>
+                    <li><a href="#">なにか</a></li>
+                    <li><a href="#">なにか</a></li>
+                    <li><a href="#">なにか</a></li>
+                </ul>
+            </section>
+
+        </aside>
+        
+    </div>
+    
+</div>
+
+<?php require 'h&f/footer1.php'; ?>
