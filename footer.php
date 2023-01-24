a4b40117a53d0936f7983819c5b76992cd49a18b
Shu <77907940+shuya1d8@users.noreply.github.com>
2023-01-24 23:43:46 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <small>
+        <!-- &copy;<strike>？ろごろご？</strike>制作会社 -->
+        &copy;-？ろごろご？-制作会社
+        <p><a href="attention.php" target="_blank">注意事項</a></p>
+    </small>
+</footer>
+</body>
+</html>
