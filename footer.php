df4ae8ae06e7a1abf0710ca10052a76e612ac147
Shu <77907940+shuya1d8@users.noreply.github.com>
2023-01-27 09:18:46 +0900

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-    <small>
-        <!-- &copy;<strike>？ろごろご？</strike>制作会社 -->
-        &copy;-？ろごろご？-制作会社
-        <p><a href="attention.php" target="_blank">注意事項</a></p>
-    </small>
-</footer>
-</body>
-</html>
